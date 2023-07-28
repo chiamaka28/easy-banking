dabd05f3721a75de3fe3c1e225c0ac40b757354d
chiamaka28 <chiamakamaduike@gmail.com>
2023-07-28 12:58:48 +0100

    fixed the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import mockup from "../assets/images/image-mockups.png";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col md:flex-row-reverse justify-between  lg:h-[65vh] my-container">
-      <div className="bg-mobile md:bg-desktop bg-left lg:bg-[right_-3rem_top_-5rem] bg-cover   bg-no-repeat py-[70px] md:py-[50px] mr-[-20%]">
+    <div className="flex flex-col md:flex-row-reverse justify-between  lg:h-[65vh] ">
+      <div className="bg-mobile md:bg-desktop bg-left lg:bg-[right_-3rem_top_-2rem] bg-cover   bg-no-repeat py-[70px] md:py-[50px] mr-[-7%]">
         <div className="w-[80%] overflow-visible mx-auto max-w-[400px] mt-[-150px]   md:mt-[-150px] md:w-[60%] md:max-w-[500px] lg:w-[80%] lg:max-w-[80rem]  lg:mr-[-20px]  lg:top-[-150px] lg:right-[-100px]  ">
           <img src={mockup} className="" alt="/" />
         </div>
       </div>
-      <div className="md:w-[40%]  lg:py-[5vh] mt-4 mb-20 flex flex-col  justify-center items-center md:justify-start md:items-start">
+      <div className="md:w-[40%] md:py-[3rem] md:px-[2rem] lg:py-[4rem] lg:mx-[8rem] mt-4 mb-20 flex flex-col  justify-center items-center md:justify-start md:items-start">
         <h1 className="text-4xl text-center  md:text-left text-bold">
           Next generation digital banking
         </h1>
